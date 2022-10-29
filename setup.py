a5487209d50243440985924a47fb7228167527c2
GarsonQuiCourt <116959105+GarsonQuiCourt@users.noreply.github.com>
2022-10-29 16:54:04 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,20 +14,10 @@ setup(
     url = 'https://github.com/GarsonQuiCourt/signals_vis', #Github link
     download_url = 'https://github.com/GarsonQuiCourt/signals_vis/archive/refs/tags/0.tar.gz',#No need to change
     keywords = ['get_url', 'download'],
-    install_requires=[
-          'beautifulsoup4',
-          'requests',
-          'selenium',
-          
-      ],
     include_package_data=True,
     long_description=readme(),
     long_description_content_type="text/markdown",
     classifiers=[
-    'Development Status :: 3 - Alpha',
-    'Intended Audience :: Developers',
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
